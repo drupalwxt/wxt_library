dc6fa0f7e356b7cc33f61f48849db3391738d2d2
William Hearn <sylus1984@gmail.com>
2017-11-05 20:29:27 -0500

    feat(condition): Taxonomy Term support for panel(izer) support

--- src/Plugin/Condition/PanelsTitle.php
+++ src/Plugin/Condition/PanelsTitle.php
@@ -4,6 +4,9 @@ namespace Drupal\wxt_library\Plugin\Condition;
 
 use Drupal\Core\Condition\ConditionPluginBase;
 use Drupal\Core\Form\FormStateInterface;
+use Drupal\Core\Plugin\ContainerFactoryPluginInterface;
+use Symfony\Component\DependencyInjection\ContainerInterface;
+use Symfony\Component\HttpFoundation\RequestStack;
 
 /**
  * Provides a 'Panels Title' condition.
@@ -13,19 +16,45 @@ use Drupal\Core\Form\FormStateInterface;
  *   label = @Translation("Panels Title"),
  *   context = {
  *     "node" = @ContextDefinition("entity:node", label = @Translation("Current Node"), required = FALSE),
+ *     "taxonomy_term" = @ContextDefinition("entity:taxonomy_term", label = @Translation("Current Taxonomy Term"), required = FALSE),
  *   }
  * )
  */
-class PanelsTitle extends ConditionPluginBase {
+class PanelsTitle extends ConditionPluginBase implements ContainerFactoryPluginInterface {
+
+  /**
+   * The request stack.
+   *
+   * @var \Symfony\Component\HttpFoundation\RequestStack
+   */
+  protected $requestStack;
+
+  /**
+   * Constructs a PanelsTitle condition plugin.
+   *
+   * @param \Symfony\Component\HttpFoundation\RequestStack $request_stack
+   *   The request stack.
+   * @param array $configuration
+   *   A configuration array containing information about the plugin instance.
+   * @param string $plugin_id
+   *   The plugin_id for the plugin instance.
+   * @param array $plugin_definition
+   *   The plugin implementation definition.
+   */
+  public function __construct(RequestStack $request_stack, array $configuration, $plugin_id, array $plugin_definition) {
+    parent::__construct($configuration, $plugin_id, $plugin_definition);
+    $this->requestStack = $request_stack;
+  }
 
   /**
    * {@inheritdoc}
    */
   public static function create(ContainerInterface $container, array $configuration, $plugin_id, $plugin_definition) {
     return new static(
-     $configuration,
-     $plugin_id,
-     $plugin_definition
+      $container->get('request_stack'),
+      $configuration,
+      $plugin_id,
+      $plugin_definition
     );
   }
 
@@ -59,6 +88,14 @@ class PanelsTitle extends ConditionPluginBase {
    */
   public function evaluate() {
 
+    // Page Manager support.
+    $request = $this->requestStack->getCurrentRequest();
+    $page_manager = $request->attributes->get('page_manager_page');
+    if (!empty($page_manager) && $page_manager->getEntityTypeId() == 'page') {
+      return FALSE;
+    }
+
+    // Panelizer support for Node.
     $node = $this->getContextValue('node');
     if (!empty($node) && $node->__isset('panelizer')) {
       $panelizer = $node->get('panelizer');
@@ -80,6 +117,32 @@ class PanelsTitle extends ConditionPluginBase {
         return FALSE;
       }
     }
+
+    // Panelizer support for Taxonomy Term.
+    $taxonomy_term = $this->getContextValue('taxonomy_term');
+    if (!empty($taxonomy_term) && $taxonomy_term->__isset('panelizer')) {
+
+      $panelizer = $taxonomy_term->get('panelizer');
+
+      $panelizer_values = $taxonomy_term->get('panelizer')->getValue();
+
+      $view_mode = array_pop($panelizer_values);
+      $view_mode = $view_mode['view_mode'];
+
+      $panelizer = \Drupal::service('panelizer');
+
+      $displays = $panelizer->getDefaultPanelsDisplays($taxonomy_term->getEntityTypeId(), $taxonomy_term->bundle(), $view_mode);
+      $display = $displays['default'];
+
+      $display = $panelizer->getEntityViewDisplay($taxonomy_term->getEntityTypeId(), $taxonomy_term->bundle(), $view_mode);
+      $render_display = $display->collectRenderDisplay($taxonomy_term, $view_mode);
+      $content = $render_display->get('content');
+
+      if (isset($content['name'])) {
+        return FALSE;
+      }
+    }
+
     return TRUE;
 
   }
