b580612dfe12c49d4066b8a821663ed7800224fa
Stephen Mulvihill <smulvih2@gmail.com>
2025-11-15 19:43:53 -0800

    Fix PHPCS issue

--- src/Plugin/Block/UserAccountBlock.php
+++ src/Plugin/Block/UserAccountBlock.php
@@ -79,7 +79,7 @@ class UserAccountBlock extends BlockBase implements ContainerFactoryPluginInterf
       $build['user_account_block']['#markup'] .= Link::fromTextAndUrl($this->t('Sign in'), Url::fromRoute('user.login', [], ['attributes' => ['class' => 'btn btn-primary']]))->toString() . "\n";
     }
     else {
-      $build['user_account_block']['#markup'] .= '<p class="mrgn-rght-sm display-inline">' . $this->t('Signed in as') . '<span class="wb-so-uname">' . ' ' . $account_name . '</span></p>';
+      $build['user_account_block']['#markup'] .= '<p class="mrgn-rght-sm display-inline">' . $this->t('Signed in as') . '<span class="wb-so-uname"> ' . $account_name . '</span></p>';
       $build['user_account_block']['#markup'] .= Link::fromTextAndUrl($this->t('Account settings'), Url::fromRoute('user.page', [], [
         'attributes' => ['class' => 'btn btn-default', 'role' => 'button'],
       ]))->toString() . "\n";
